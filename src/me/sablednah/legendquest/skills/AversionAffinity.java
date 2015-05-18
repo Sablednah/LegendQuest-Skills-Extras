49f532efeb934054ec4d92cbb7b5a00455fcec48
Darren Douglas <darren.douglas@gmail.com>
2015-05-18 17:11:47 +0100

    Upgraes to aversion affinity and biomesensitive to allow effects

--- src/me/sablednah/legendquest/skills/AversionAffinity.java
+++ src/me/sablednah/legendquest/skills/AversionAffinity.java
@@ -1,7 +1,11 @@
 package me.sablednah.legendquest.skills;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
+import me.sablednah.legendquest.effects.EffectProcess;
+import me.sablednah.legendquest.effects.Effects;
+import me.sablednah.legendquest.effects.OwnerType;
 import me.sablednah.legendquest.events.AbilityCheckEvent;
 import me.sablednah.legendquest.events.SkillTick;
 import me.sablednah.legendquest.events.SpeedCheckEvent;
@@ -13,15 +17,19 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.potion.PotionEffect;
 
-@SkillManifest(name = "AversionAffinity", type = SkillType.PASSIVE, author = "SharksterBoy", version = 1.0D, 
+@SkillManifest(name = "AversionAffinity", type = SkillType.PASSIVE, author = "SharksterBoy", version = 2.0D, 
 description = "Damage when walking on [harmfullmaterials], heal on [healingmaterials]", 
 consumes = "", manaCost = 0, levelRequired = 0, skillPoints = 0, 
 buildup = 0, delay = 0, duration = 0, cooldown = 0, 
-dblvarnames = { "regenerate", "damage","harmspeed","healspeed" }, dblvarvalues = { 1.0,1.0, 0.0,0.0 }, 
+dblvarnames = { "regenerate", "damage","harmspeed","healspeed" }, 
+dblvarvalues = { 1.0,1.0, 0.0,0.0 }, 
 intvarnames = { "regeninterval","damageinterval","healbonus","harmpenalty" }, 
 intvarvalues = { 5, 5, 1, 1 }, 
-strvarnames = { "harmfullmaterials", "healingmaterials" }, strvarvalues = { "SOUL_SAND","GRASS" })
+strvarnames = { "harmfullmaterials", "healingmaterials", "healeffects", "harmeffects" }, 
+strvarvalues = { "SOUL_SAND","GRASS", "", "" }
+)
 public class AversionAffinity extends Skill implements Listener {
 	public boolean onEnable() {
 		return true;
@@ -80,17 +88,87 @@ public class AversionAffinity extends Skill implements Listener {
 			heallist.add(Material.matchMaterial(s));
 		}
 		
-		if (damage>0.0D) {
-			if (harmlist.contains(b.getType())) { // dark
+		if (harmlist.contains(b.getType())) { // dark
+			if (damage>0.0D) {
 				if ((lq.players.ticks % ((damageinterval*20)/lq.configMain.skillTickInterval)) == 0 ) {
 					getPC(p).damage(damage);
 				}
 			}
+			String effects = ((String) data.vars.get("healeffects"));
+			if (effects != null && !effects.isEmpty()) {
+				String[] listy = effects.split("\\s*,\\s*");
+				for (String s : listy) {
+					Effects ef = Effects.valueOf(s.toUpperCase());
+					int dur = (damageinterval * 1000);
+					if (dur < 5000) {
+						dur = 5000;
+					}
+					EffectProcess ep = null;
+					ep = new EffectProcess(ef, dur, OwnerType.PLAYER, p.getUniqueId());
+//System.out.print("Duration: " + dur);
+					if (p.hasPotionEffect(ef.getPotioneffectType())) {
+//System.out.print("has: " + ef.getPotioneffectType().toString());
+						boolean isshorter = false;
+						Collection<PotionEffect> pots = p.getActivePotionEffects();
+						for (PotionEffect pe : pots) {
+							if (pe.getType().equals(ef.getPotioneffectType())) {
+								if (pe.getDuration() < lq.configMain.skillTickInterval) {
+									isshorter = true;
+//System.out.print("effect Duration shorter: " + pe.getDuration() + " < " + lq.configMain.skillTickInterval);
+								}
+							}
+						}
+						if (isshorter) {
+							lq.effectManager.removeEffects(OwnerType.PLAYER, p.getUniqueId(), ef);
+							p.removePotionEffect(ef.getPotioneffectType());
+							lq.effectManager.addPendingProcess(ep);
+						}
+					} else {
+//System.out.print("NOT has: " + ef.getPotioneffectType().toString());
+						lq.effectManager.addPendingProcess(ep);
+					}
+				}
+			}
 		}
-		if (regenerate>0.0D) {
-			if (heallist.contains(b.getType())) { // dark
+		if (heallist.contains(b.getType())) { // dark
+			if (regenerate>0.0D) {
 				if ((lq.players.ticks % ((regeninterval*20)/lq.configMain.skillTickInterval)) == 0 ) {
 					getPC(p).heal(regenerate);
+				}
+			}
+			String effects = ((String) data.vars.get("healeffects"));
+			if (effects != null && !effects.isEmpty()) {
+				String[] listy = effects.split("\\s*,\\s*");
+				for (String s : listy) {
+					Effects ef = Effects.valueOf(s.toUpperCase());
+					int dur = (damageinterval * 1000);
+					if (dur < 5000) {
+						dur = 5000;
+					}
+					EffectProcess ep = null;
+					ep = new EffectProcess(ef, dur, OwnerType.PLAYER, p.getUniqueId());
+//System.out.print("Duration: " + dur);
+					if (p.hasPotionEffect(ef.getPotioneffectType())) {
+//System.out.print("has: " + ef.getPotioneffectType().toString());
+						boolean isshorter = false;
+						Collection<PotionEffect> pots = p.getActivePotionEffects();
+						for (PotionEffect pe : pots) {
+							if (pe.getType().equals(ef.getPotioneffectType())) {
+								if (pe.getDuration() < lq.configMain.skillTickInterval) {
+									isshorter = true;
+//System.out.print("effect Duration shorter: " + pe.getDuration() + " < " + lq.configMain.skillTickInterval);
+								}
+							}
+						}
+						if (isshorter) {
+							lq.effectManager.removeEffects(OwnerType.PLAYER, p.getUniqueId(), ef);
+							p.removePotionEffect(ef.getPotioneffectType());
+							lq.effectManager.addPendingProcess(ep);
+						}
+					} else {
+//System.out.print("NOT has: " + ef.getPotioneffectType().toString());
+						lq.effectManager.addPendingProcess(ep);
+					}
 				}
 			}
 		}
--- src/me/sablednah/legendquest/skills/BiomeSesnsitive.java
+++ src/me/sablednah/legendquest/skills/BiomeSesnsitive.java
@@ -1,7 +1,11 @@
 package me.sablednah.legendquest.skills;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
+import me.sablednah.legendquest.effects.EffectProcess;
+import me.sablednah.legendquest.effects.Effects;
+import me.sablednah.legendquest.effects.OwnerType;
 import me.sablednah.legendquest.events.AbilityCheckEvent;
 import me.sablednah.legendquest.events.SkillTick;
 import me.sablednah.legendquest.events.SpeedCheckEvent;
@@ -11,15 +15,19 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.potion.PotionEffect;
 
-@SkillManifest(name = "BiomeSensitive", type = SkillType.PASSIVE, author = "SableDnah", version = 1.0D, 
+@SkillManifest(name = "BiomeSensitive", type = SkillType.PASSIVE, author = "SableDnah", version = 2.0D, 
 description = "Multiple effects dependent on biome type", 
 consumes = "", manaCost = 0, levelRequired = 0, skillPoints = 0, 
 buildup = 0, delay = 0, duration = 0, cooldown = 0, 
-dblvarnames = { "regenerate", "damage","harmspeed","healspeed" }, dblvarvalues = { 1.0,1.0,0.0,0.0 }, 
+dblvarnames = { "regenerate", "damage","harmspeed","healspeed" }, 
+dblvarvalues = { 1.0,1.0,0.0,0.0 }, 
 intvarnames = { "regeninterval","damageinterval","healbonus","harmpenalty" }, 
 intvarvalues = { 5, 5, 1, 1 }, 
-strvarnames = { "message", "harmfullbiomes", "healingbiomes" }, strvarvalues = { "Tuning to nature...","HELL","FLOWER_FOREST" })
+strvarnames = { "message", "harmfullbiomes", "healingbiomes", "harmeffects", "healeffects" }, 
+strvarvalues = { "Tuning to nature...","HELL","FLOWER_FOREST", "", "" }
+)
 
 // see https://hub.spigotmc.org/javadocs/spigot/org/bukkit/block/Biome.html
 
@@ -94,17 +102,87 @@ public class BiomeSesnsitive extends Skill implements Listener {
 				}
 			}
 			
-			if (damage>0.0D) {
-				if (harmlist.contains(b)) { // dark
+			if (harmlist.contains(b)) { // dark
+				if (damage>0.0D) {
 					if ((lq.players.ticks % ((damageinterval*20)/lq.configMain.skillTickInterval)) == 0 ) {
 						getPC(p).damage(damage);
 					}
 				}
+				String effects = ((String) data.vars.get("harmeffects"));
+				if (effects != null && !effects.isEmpty()) {
+					String[] listy = effects.split("\\s*,\\s*");
+					for (String s : listy) {
+						Effects ef = Effects.valueOf(s.toUpperCase());
+						int dur = (damageinterval * 1000);
+						if (dur < 5000) {
+							dur = 5000;
+						}
+						EffectProcess ep = null;
+						ep = new EffectProcess(ef, dur, OwnerType.PLAYER, p.getUniqueId());
+//System.out.print("Duration: " + dur);
+						if (p.hasPotionEffect(ef.getPotioneffectType())) {
+//System.out.print("has: " + ef.getPotioneffectType().toString());
+							boolean isshorter = false;
+							Collection<PotionEffect> pots = p.getActivePotionEffects();
+							for (PotionEffect pe : pots) {
+								if (pe.getType().equals(ef.getPotioneffectType())) {
+									if (pe.getDuration() < lq.configMain.skillTickInterval) {
+										isshorter = true;
+//System.out.print("effect Duration shorter: " + pe.getDuration() + " < " + lq.configMain.skillTickInterval);
+									}
+								}
+							}
+							if (isshorter) {
+								lq.effectManager.removeEffects(OwnerType.PLAYER, p.getUniqueId(), ef);
+								p.removePotionEffect(ef.getPotioneffectType());
+								lq.effectManager.addPendingProcess(ep);
+							}
+						} else {
+//System.out.print("NOT has: " + ef.getPotioneffectType().toString());
+							lq.effectManager.addPendingProcess(ep);
+						}
+					}
+				}
 			}
-			if (regenerate>0.0D) {
-				if (heallist.contains(b)) { // dark
+			if (heallist.contains(b)) { // dark
+				if (regenerate>0.0D) {
 					if ((lq.players.ticks % ((regeninterval*20)/lq.configMain.skillTickInterval)) == 0 ) {
 						getPC(p).heal(regenerate);
+					}
+				}
+				String effects = ((String) data.vars.get("healeffects"));
+				if (effects != null && !effects.isEmpty()) {
+					String[] listy = effects.split("\\s*,\\s*");
+					for (String s : listy) {
+						Effects ef = Effects.valueOf(s.toUpperCase());
+						int dur = (damageinterval * 1000);
+						if (dur < 5000) {
+							dur = 5000;
+						}
+						EffectProcess ep = null;
+						ep = new EffectProcess(ef, dur, OwnerType.PLAYER, p.getUniqueId());
+//System.out.print("Duration: " + dur);
+						if (p.hasPotionEffect(ef.getPotioneffectType())) {
+//System.out.print("has: " + ef.getPotioneffectType().toString());
+							boolean isshorter = false;
+							Collection<PotionEffect> pots = p.getActivePotionEffects();
+							for (PotionEffect pe : pots) {
+								if (pe.getType().equals(ef.getPotioneffectType())) {
+									if (pe.getDuration() < lq.configMain.skillTickInterval) {
+										isshorter = true;
+//System.out.print("effect Duration shorter: " + pe.getDuration() + " < " + lq.configMain.skillTickInterval);
+									}
+								}
+							}
+							if (isshorter) {
+								lq.effectManager.removeEffects(OwnerType.PLAYER, p.getUniqueId(), ef);
+								p.removePotionEffect(ef.getPotioneffectType());
+								lq.effectManager.addPendingProcess(ep);
+							}
+						} else {
+//System.out.print("NOT has: " + ef.getPotioneffectType().toString());
+							lq.effectManager.addPendingProcess(ep);
+						}
 					}
 				}
 			}
